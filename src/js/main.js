304de827ef044c4988b3e548a7f608e079ee7b98
Gabriel Barreto <88603031+Cyacer@users.noreply.github.com>
2022-12-21 01:45:10 -0300

    Modo light completo

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+const body = document.querySelector("body");
+const toggle = document.querySelector("#toggle");
+const sunIcon = document.querySelector(".toggle .bxs-sun");
+const moonIcon = document.querySelector(".toggle .bx-moon");
+toggle.addEventListener("change", (e) => {
+    
+    body.classList.toggle("dark", e.target.checked);
+    sunIcon.className = sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
+    moonIcon.className = moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
+});
